72c60a1be286c16b6528bf81b4d56ac1e785001e
JasminHT <jasminht@gmail.com>
2026-02-19 12:43:43 -0500

    Added an API key in secret env

--- FlaskGPT/app.py
+++ FlaskGPT/app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request, jsonify
 from openai import OpenAI
 import os
 
+# importing necessary functions from dotenv library
+from dotenv import load_dotenv, dotenv_values 
+# loading variables from .env file
+load_dotenv() 
+
 app = Flask(__name__)
 
 # Set your OpenAI API key as environment variable for safety
@@ -9,16 +14,17 @@ client = OpenAI()
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    test_key = os.getenv("TEST_KEY")
+    return render_template('index.html',data=test_key)
 
 @app.route('/chat', methods=['POST'])
 def chat():
     user_message = request.json.get('message')
 
     response = client.chat.completions.create(
-        model='gpt-4o-mini',  
+        model='gpt-5-nano',  # gpt-5-nano is the cheapest model right now
         messages=[{'role': 'user', 'content': user_message}],
-        max_tokens=200,
+        max_tokens=20,
         temperature=0.4
     )
 
